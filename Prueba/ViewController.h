403c5f1abfcc8e4de018e86acc191dafaf7ca318
curso <usuariocurso@iMac-de-Francisco.local>
2013-11-28 10:02:37 -0500

    Initial Commit

---
+++ Prueba/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Prueba
+//
+//  Created by curso on 28/11/13.
+//  Copyright (c) 2013 Pato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Prueba/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Prueba
+//
+//  Created by curso on 28/11/13.
+//  Copyright (c) 2013 Pato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
