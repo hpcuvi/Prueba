16c1cfea8a3d388a54c6b36815f341c6bd7c06b1
Pato <hpcuvi@gmail.com>
2013-12-24 10:27:24 -0500

    Carga de proyecto inicial

---
+++ Prueba/TablaViewController.h
@@ -0,0 +1,13 @@
+//
+//  TablaViewController.h
+//  Prueba
+//
+//  Created by curso on 28/11/13.
+//  Copyright (c) 2013 Pato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TablaViewController : UITableViewController
+
+@end
