6457a074d0742dbbe1c138ed38223449d4caf751
Alejandro Barrionuevo <alex_neighbournew@hotmail.com>
2013-12-26 09:22:59 -0500

    comit

--- Prueba/ViewController.m
+++ Prueba/ViewController.m
@@ -19,7 +19,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     // estoy modificando huyfghjfgh
-    NSString *nombre;
+    NSString *nombre; // variable nombre
     // aksdjfkasdfsd
 }
 
